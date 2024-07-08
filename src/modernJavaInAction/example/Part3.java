285bc42f5433913ddf1fd8e3ede0d90346f03442
33l33q <22bmad@gmail.com>
2024-07-08 23:54:44 +0900

    20240708 study

--- src/modernJavaInAction/example/Part3.java
+++ src/modernJavaInAction/example/Part3.java
@@ -3,7 +3,11 @@ package modernJavaInAction.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+
 import modernJavaInAction.example.Part1.Apple;
 
 public class Part3 {
@@ -51,10 +55,59 @@ public class Part3 {
 		}
 	}
 	
-		
+	//함수형 인터페이스 사용할 때
+	public List<String> listOfString = new ArrayList<String>();
+	
+/*	@FunctionalInterface
+	public interface Predicate<T>{
+		boolean test(T t);
+	}
+	
+	Predicate<String> nonEmptyStringPredicate = (String s) -> !s.isEmpty();
+	
+	public <T> List<T> filter(List<T> list, Predicate<T> p){
+		List<T> results = new ArrayList<>();
+		for(T t : list) {
+			if(p.test(t)) {
+				results.add(t);
+			}
+		}
+		return results;
+	}
+	
+	List nonEmpty = filter(listOfString, nonEmptyStringPredicate); */
+	
+	@FunctionalInterface
+	public interface IntPredicate{
+		boolean test(int t);
+	}
+	
+	IntPredicate evenNumbers = (int i) -> {return i % 2 == 0;};
+
+	@FunctionalInterface
+	public interface Predicate<Integer>{
+		boolean test(int t);
+	}
+	
+	Predicate<Integer> evenNumbers2 = (int i) -> {return i % 2 == 0;};
+	
+	
+	@FunctionalInterface
+	public interface Function<T, R>{
+		R apply(T t);
+	}
+	
+	public <T, R> List<R> map2(List<T> list, Function <T, R> f){
+		List<R> result = new ArrayList<>();
+		for(T t : list) {
+			result.add(f.apply(t));
+		}
+		return result;
+	}
+	
 	
 	public static void main(String[] args) {
-		
+		Part3 part3 = new Part3();
 
 		Runnable r1 = () -> System.out.println("hello world");
 		Runnable r2 = new Runnable() {
@@ -74,6 +127,8 @@ public class Part3 {
 
 		}	
 	
+		List<Integer> l = part3.map2(Arrays.asList("lambdas","in","action"),(String s) -> s.length());
+		System.out.println(l);
 	}
 	
 }
